c8a50dcfb56cbae80e57b0ccd806ef4b6704dcbb
Alon Dahari <climbinghobo@gmail.com>
2015-02-11 14:10:44 -0700

    add id to view, moved fields to view

--- app/js/routers/router.js
+++ app/js/routers/router.js
@@ -22,33 +22,8 @@ define([
 
 		spot: function(id){
 			// must be a better way to do this!
-			var ratings = new Ratings([
-				{
-					header: 'Overall Wave Quality',
-					defaultValue: 4.2,
-					max: 10,
-					unit: '/ 10'
-				},
-				{
-					header: 'Wave Height',
-					defaultValue: 6.2,
-					max: 12,
-					unit: 'ft'
-				},
-				{
-					header: 'Wind',
-					defaultValue: 0,
-					max: 5,
-					unit: 'mph'
-				},
-				{
-					header: 'Crowd',
-					defaultValue: 200,
-					max: 200,
-					unit: 'surfers'
-				}
-			]);
-			new RateView({ model: ratings, header: 'id' });
+			var ratings = new Ratings();
+			new RateView({ model: ratings, id: id });
 		}
 
 	});
--- app/js/views/rate.js
+++ app/js/views/rate.js
@@ -11,6 +11,33 @@ define([
 
 		template: _.template(template),
 
+		fields: [
+			{
+				header: 'Overall Wave Quality',
+				defaultValue: 4.2,
+				max: 10,
+				unit: '/ 10'
+			},
+			{
+				header: 'Wave Height',
+				defaultValue: 6.2,
+				max: 12,
+				unit: 'ft'
+			},
+			{
+				header: 'Wind',
+				defaultValue: 0,
+				max: 5,
+				unit: 'mph'
+			},
+			{
+				header: 'Crowd',
+				defaultValue: 200,
+				max: 200,
+				unit: 'surfers'
+			}
+		],
+
 		events: {
 
 		},
@@ -22,7 +49,7 @@ define([
 		render: function(){
 			this.$el.html(this.template());
 			// this.$el.find('.ratings').html()
-			console.log(this.header);
+			console.log(this.id);
 		}
 
 
