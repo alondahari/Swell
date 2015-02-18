b88810dab32e6d0fa948bcd120f5648d627a6d7f
Alon Dahari <climbinghobo@gmail.com>
2015-02-18 10:51:27 -0700

    test for filterRatings

--- app/test/spec/collections/ratings.js
+++ app/test/spec/collections/ratings.js
@@ -1,18 +1,63 @@
 define([
-  '/app/js/collections/ratings.js'
+  '/app/js/collections/ratings.js',
   ], function (
   	Ratings
   ) {
 
 	describe("ratings collection", function() {
+
+		beforeEach(function(){
+			ratings = new Ratings()
+		})
 		
 		it("should fetch all ratings on init", function() {
-			var localStorageKeys = _.keys(localStorage);
+			var localStorageKeys = _.keys(localStorage)
 			var localRatings = _.filter(localStorageKeys, function(val){
 				return val.indexOf('ratings-') >= 0
 			})
-			ratings = new Ratings();
-			expect(ratings.length).toEqual(localRatings.length);
+			expect(ratings.length).toEqual(localRatings.length)
+		});
+
+		it("should filter out irrelevant ratings", function() {
+			
+			var cutOff = 100,
+				field = "overall",
+				spot_name = "Ward Avenue",
+				fakeRatings = [
+				// different spot - filter out
+				{
+					crowd: 51,
+					overall: 10,
+					spot_name: "County Line",
+					time: 101,
+					waveHeight: 6,
+					wind: 2
+				},
+				// no overall field - filter out
+				{
+					crowd: 51,
+					spot_name: "Ward Avenue",
+					time: 101
+				},
+				// too old - filter out
+				{
+					overall: 2,
+					spot_name: "Ward Avenue",
+					time: 99
+				},
+				// keep
+				{
+					overall: 5,
+					spot_name: "Ward Avenue",
+					time: 101
+				}
+			]
+			ratings.filterRatings.bind(fakeRatings);
+			var filtered = ratings.filterRatings(cutOff, spot_name, field)
+			
+			expect(filtered.length).toBe(1)
+			expect(filtered[0].get('overall')).toBe(5)
+
 		});
 
 
