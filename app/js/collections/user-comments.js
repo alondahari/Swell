6d33dc64129b045e4185b2b407d3cfd6f49b3176
Alon Dahari <climbinghobo@gmail.com>
2015-03-16 12:29:36 -0600

    remove app folder

--- app/js/collections/user-comments.js
+++
@@ -1,9 +0,0 @@
-define(['backbone', 'models/user-comment'], function(Backbone, userComment){
-
-	return Backbone.Model.extend({
-		idAttribute: '_id',
-		model: 'userComment',
-		url: backendPath + '/comments'
-	})
-
-})
